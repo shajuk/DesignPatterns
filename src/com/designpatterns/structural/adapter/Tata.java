c48f782b32f9a9666922eaddc319c8b8be51550f
shajuk <shajuk@users.noreply.github.com>
2017-01-03 17:44:08 +0530

    Create Tata.java

---
+++ src/com/designpatterns/structural/adapter/Tata.java
@@ -0,0 +1,10 @@
+package com.designpatterns.structural.bridge;
+
+public class Tata implements Manufacturer {
+
+	@Override
+	public void showName() {
+		System.out.println("Manufacturer: Tata");
+	}
+
+}
