46d5bdb125bcc72ce93bc8c7d6be2edf84037061
shajuk <shajuk@users.noreply.github.com>
2017-01-03 17:43:48 +0530

    Create Maruthi.java

---
+++ src/com/designpatterns/structural/adapter/Maruthi.java
@@ -0,0 +1,10 @@
+package com.designpatterns.structural.bridge;
+
+public class Maruthi implements Manufacturer {
+
+	@Override
+	public void showName() {
+		System.out.println("Manufacturer: Maruthi");
+	}
+
+}
