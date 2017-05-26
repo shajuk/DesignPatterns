be822b1142404696572ae640e757eb415e4ac709
shajuk <shaju.shajuk@gmail.com>
2017-05-26 08:43:52 +0530

    commit test

--- src/com/designpatterns/creational/factory/FactoryPatternDemo.java
+++ src/com/designpatterns/creational/factory/FactoryPatternDemo.java
@@ -2,6 +2,7 @@ package com.designpatterns.creational.factory;
 
 public class FactoryPatternDemo {
 	public static void main(String[] args) {
+		System.out.println("Factory pattern demo");
 		Products e=ProductFactory.getProducts(ProductType.ELECTRONICS);
 		System.out.println(e.getItems());
 		Products s=ProductFactory.getProducts(ProductType.SPORTS);
